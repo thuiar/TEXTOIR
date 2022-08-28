c6b7f8d611f1367ca2e2b0b78588872a08dea9bc
Hanlei Zhang <zhanghanlei98@gmail.com>
2022-08-28 12:35:41 +0800

    Update functions.py

--- open_intent_detection/utils/functions.py
+++ open_intent_detection/utils/functions.py
@@ -104,25 +104,3 @@ def euclidean_metric(a, b):
     b = b.unsqueeze(0).expand(n, m, -1)
     logits = -((a - b)**2).sum(dim=2)
     return logits
-
-def sigmoid_rampup(current, rampup_length):
-    """Exponential rampup from https://arxiv.org/abs/1610.02242"""
-    if rampup_length == 0:
-        return 1.0
-    else:
-        current = np.clip(current, 0.0, rampup_length)
-        phase = 1.0 - current / rampup_length
-        return float(np.exp(-5.0 * phase * phase))
-
-def linear_rampup(current, rampup_length):
-    """Linear rampup"""
-    assert current >= 0 and rampup_length >= 0
-    if current >= rampup_length:
-        return 1.0
-    else:
-        return current / rampup_length
-
-def cosine_rampdown(current, rampdown_length):
-    """Cosine rampdown from https://arxiv.org/abs/1608.03983"""
-    assert 0 <= current <= rampdown_length
-    return max(float(.5 * (np.cos(np.pi * current / rampdown_length) + 1)), 0.5)
