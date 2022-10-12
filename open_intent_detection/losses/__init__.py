8964e54eb10e338e8fd2a359331ec81a45681d6c
P1ut0 <murrayzhao@163.com>
2022-10-12 09:33:18 +0800

    Update __init__.py

--- open_intent_detection/losses/__init__.py
+++ open_intent_detection/losses/__init__.py
@@ -3,5 +3,6 @@ from torch import nn
 
 loss_map = {
                 'CrossEntropyLoss': nn.CrossEntropyLoss(), 
-                'Binary_CrossEntropyLoss': nn.BCELoss()
+                'Binary_CrossEntropyLoss': nn.BCELoss(),
+                'CosineFaceLoss': CosineFaceLoss()
             }
